3184e900392c0c631728985373b6fbf66392fe87
nomura0928 <167419142+nomura0928@users.noreply.github.com>
2026-04-10 22:46:37 +0900

    検索機能をinfomation.jsxに移動

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import { useEffect, useRef, useState } from 'react'
 import { BrowserRouter, Link, Route, Routes, Navigate } from 'react-router-dom'
 import './App.css'
 import Information from './Information';
 import FavoriteList from './FavoriteList';
 
 function App() {
-  const apikey = import.meta.env.VITE_OMDB_API_KEY
-  const filmName = useRef();
-  const filmYear = useRef();
-  const [filmInfo, setInfo] = useState(null);
-
-  const getInfo = async (e) => {
-    e.preventDefault();
-    const name = filmName.current.value;
-    const year = filmYear.current.value;
-    if (!name) return;
-    try {
-      //awaitで非同期処理が完了するまで待つ
-      const res = await fetch(`https://www.omdbapi.com/?apikey=${apikey}&t=${name}&y=${year}`);
-      console.log(res);
-      const data = await res.json();
-      setInfo(data);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  useEffect(() => console.log(filmInfo), [filmInfo]);
-
   return (
     <>
-    {/*ルーティング機能を有効にする範囲を囲む　Link,Route*/}
+      {/*ルーティング機能を有効にする範囲を囲む　Link,Route*/}
       <BrowserRouter>
         <header>
           <h1>Video Search</h1>
@@ -40,30 +16,13 @@ function App() {
             <Link className='link' to={'/favorites'}>お気に入り</Link>
           </nav>
         </header>
-        <form onSubmit={getInfo} className='search-section'>
-          <div className="input-left">
-            <label className='search-div'>
-              <span className='input-info'>タイトル</span>
-              <input type="text" ref={filmName} placeholder='英語で入力' />
-            </label>
-            <label className='search-div'>
-              <span className='input-info'>公開年</span>
-              <input type="text" ref={filmYear} />
-            </label>
-          </div>
-          <div className="input-right">
-            <button className='button' type='submit'>Search</button>
-          </div>
-        </form>
-        <section className='film-info'>
-          <Routes>
-            {/* リンクごとに表示する内容を切り替える */}
-            {/* NavigateでURLを自動的に変更 */}
+        <Routes>
+          {/* リンクごとに表示する内容を切り替える */}
+          {/* NavigateでURLを自動的に変更 */}
           <Route path='/' element={<Navigate to='/search'/>} />
-          <Route path='/search' element={<Information data={filmInfo} />} />
+          <Route path='/search' element={<Information />} />
           <Route path='/favorites' element={<FavoriteList/>} />
-          </Routes>
-        </section>
+        </Routes>
       </BrowserRouter>
     </>
   )
