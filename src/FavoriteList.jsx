6da1912ef3e404c024a07b099baa7e86cb203d03
nomura0928 <167419142+nomura0928@users.noreply.github.com>
2026-04-11 14:57:58 +0900

    お気に入りリストの映画を表示するように変更

--- src/FavoriteList.jsx
+++ src/FavoriteList.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FavoriteList = () => {
+  const [favorites,setFavorites] = useState([]);
 
+  useEffect(() => {
+    try {
+      fetch(`http://localhost:8080/favorites`)
+      .then(res => res.json())
+      .then(data => setFavorites(data))
+    } catch (err) {
+      console.log(err);
+    }
+  },[])
 
-  
   return (
     <div>
-
+      {favorites.map((favorites) =>
+      <div>
+        <img src={favorites.Poster} alt="Poster" onError={(e) => e.target.src = '/src/assets/noimage.jpg'}/>
+        <h4>{favorites.Title}</h4>
+        <h4>{favorites.Year}</h4>
+      </div>
+      )}
     </div>
   )
 }
